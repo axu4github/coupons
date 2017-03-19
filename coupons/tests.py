ece2aa8ad4a28db7739162101b3f2a252d79398c
axu4github <axu.home@gmail.com>
2017-03-19 10:56:25 +0800

    添加针对加载自定义配置文件的测试

--- coupons/tests.py
+++ coupons/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# -*- coding:utf-8 -*-
 
-# Create your tests here.
+"""
+单元测试
+
+"""
+
+from django.test import TestCase
+from django.conf import settings
+
+
+class EnvTest(TestCase):
+
+    def test_load_custom_settings(self):
+        """
+        测试修改了配置文件目录（由默认创建的'${BASE_DIR}/coupons'修改为${BASE_DIR}/'configs'）是否可以顺利加载配置
+        """
+
+        import os
+        self.assertIsNotNone(settings.SECRET_KEY)
+        self.assertEqual(settings.BASE_DIR, os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))))
