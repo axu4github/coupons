719672aad74d6988ab689a587fe7e106fbe7b2d0
anxu <axu.home@gmail.com>
2017-03-19 13:01:05 +0800

    添加创建'Coupons'表一条记录的测试

--- coupons/tests.py
+++ coupons/tests.py
@@ -7,6 +7,7 @@
 
 from django.test import TestCase
 from django.conf import settings
+from coupons.models import Coupons
 
 
 class EnvTest(TestCase):
@@ -20,3 +21,29 @@ class EnvTest(TestCase):
         self.assertIsNotNone(settings.SECRET_KEY)
         self.assertEqual(settings.BASE_DIR, os.path.dirname(
             os.path.dirname(os.path.abspath(__file__))))
+
+
+class DBTest(TestCase):
+    """
+    数据库测试
+    """
+
+    def test_coupons_create_one_row(self):
+        # 从数据库中取出的中文数据是`Unicode`编码的，所以为了后面的验证，这里也要使用`Unicode`编码声明。
+        name = u"测试_添加优惠券表_一行_名称"
+        describe = u"测试_添加优惠券表_一行_描述"
+        url = "http://www.test.com/add_one_row_by_url"
+        classification = u"测试_添加优惠券表_一行_分类"
+
+        Coupons.objects.create(
+            name=name,
+            describe=describe,
+            url=url,
+            classification=classification)
+
+        coupon = Coupons.objects.get(name=name)
+
+        self.assertEqual(coupon.describe, describe)
+        self.assertEqual(coupon.url, url)
+        self.assertEqual(coupon.classification, classification)
+        self.assertTrue(coupon.enabled)
